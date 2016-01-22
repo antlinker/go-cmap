9343bd7a6a4e4e42240ff30f704c9a1b6f67b4c6
lyric <tiannianshou@gmail.com>
2016-01-22 18:44:34 +0800

    Add get keys,values method;Modify test.

--- cmap.go
+++ cmap.go
@@ -18,22 +18,26 @@ type ConcurrencyMap interface {
 	Get(key interface{}) (interface{}, error)
 	// Set 给指定的键设置元素值。若该键值已存在，则替换
 	Set(key interface{}, elem interface{}) error
-	// SetIfAbsent 给指定的键设置元素值。若该键值已存在，则不替换,并返回以存在的值
-	//返回值 value 为执行方法后key 键对应的实际值
-	//返回值isnew true是新值，false是原来的值
-	SetIfAbsent(key interface{}, elem interface{}) (value interface{}, isnew bool)
+	// SetIfAbsent 给指定的键设置元素值。
+	// 若该键值已存在，则不替换,并返回已经存在的值同时返回false
+	// 若改键值不存在，则增加该键值同时返回true
+	SetIfAbsent(key interface{}, elem interface{}) (interface{}, bool)
 	// Remove 删除给定键值对应的键值对，并返回旧的元素值。若没有旧元素的值则返回nil
 	Remove(key interface{}) (interface{}, error)
 	// Contains 判断是否包含给定的键值
 	Contains(key interface{}) (bool, error)
-	// GetElement 获取并发Map中的元素
-	GetElement() <-chan ConcurrencyElement
-	// ToMap 获取已包含的键值对所组成的字典值
-	ToMap() map[interface{}]interface{}
 	// Clear 清除所有的键值对
 	Clear()
 	// Len 获取键值对的数量
 	Len() int
+	// ToMap 获取已包含的键值对所组成的字典值
+	ToMap() map[interface{}]interface{}
+	// Elements 获取并发Map中的元素
+	Elements() <-chan ConcurrencyElement
+	// Keys 获取所有的键数据
+	Keys() []interface{}
+	// Values 获取所有的值数据
+	Values() []interface{}
 }
 
 // NewConcurrencyMap 创建并发的Map接口
@@ -57,7 +61,6 @@ func NewConcurrencyMap(poolSizes ...uint) ConcurrencyMap {
 	}
 }
 
-// ConcurrencyElement
 type ConcurrencyElement struct {
 	Key   interface{}
 	Value interface{}
@@ -69,6 +72,7 @@ type concurrencyItem struct {
 }
 
 type concurrencyMap struct {
+	sync.RWMutex
 	size  int
 	pools []*concurrencyItem
 }
@@ -157,35 +161,6 @@ func (cm *concurrencyMap) Contains(key interface{}) (bool, error) {
 	return ok, nil
 }
 
-func (cm *concurrencyMap) GetElement() <-chan ConcurrencyElement {
-	chElement := make(chan ConcurrencyElement)
-	go func() {
-		for i := 0; i < cm.size; i++ {
-			item := cm.pools[i]
-			item.RLock()
-			for k, v := range item.items {
-				chElement <- ConcurrencyElement{Key: k, Value: v}
-			}
-			item.RUnlock()
-		}
-		close(chElement)
-	}()
-	return chElement
-}
-
-func (cm *concurrencyMap) ToMap() map[interface{}]interface{} {
-	data := make(map[interface{}]interface{})
-	for i := 0; i < cm.size; i++ {
-		item := cm.pools[i]
-		item.RLock()
-		for k, v := range item.items {
-			data[k] = v
-		}
-		item.RUnlock()
-	}
-	return data
-}
-
 func (cm *concurrencyMap) Clear() {
 	for i := 0; i < cm.size; i++ {
 		item := cm.pools[i]
@@ -204,4 +179,59 @@ func (cm *concurrencyMap) Len() int {
 		item.RUnlock()
 	}
 	return count
+}
+
+func (cm *concurrencyMap) ToMap() map[interface{}]interface{} {
+	data := make(map[interface{}]interface{})
+	for i := 0; i < cm.size; i++ {
+		item := cm.pools[i]
+		item.RLock()
+		for k, v := range item.items {
+			data[k] = v
+		}
+		item.RUnlock()
+	}
+	return data
+}
+
+func (cm *concurrencyMap) Elements() <-chan ConcurrencyElement {
+	chElement := make(chan ConcurrencyElement)
+	go func() {
+		for i := 0; i < cm.size; i++ {
+			item := cm.pools[i]
+			item.RLock()
+			for k, v := range item.items {
+				chElement <- ConcurrencyElement{Key: k, Value: v}
+			}
+			item.RUnlock()
+		}
+		close(chElement)
+	}()
+	return chElement
+}
+
+func (cm *concurrencyMap) Keys() []interface{} {
+	var keys []interface{}
+	for i := 0; i < cm.size; i++ {
+		item := cm.pools[i]
+		item.RLock()
+		for k, _ := range item.items {
+			keys = append(keys, k)
+		}
+		item.RUnlock()
+	}
+	return keys
+}
+
+func (cm *concurrencyMap) Values() []interface{} {
+	var values []interface{}
+	for i := 0; i < cm.size; i++ {
+		item := cm.pools[i]
+		item.RLock()
+		for _, v := range item.items {
+			values = append(values, v)
+		}
+		item.RUnlock()
+	}
+	return values
 }
--- cmap_test.go
+++ cmap_test.go
@@ -1,129 +1,40 @@
-package cmap_test
+package cmap
 
 import (
 	"fmt"
-	"runtime"
-	"sync"
-	"sync/atomic"
 	"testing"
-
-	. "github.com/antlinker/go-cmap"
 )
 
-func TestGetElement(t *testing.T) {
+func TestCMap(t *testing.T) {
 	cmap := NewConcurrencyMap()
-	cmap.Set("foo", "bar")
-	for ele := range cmap.GetElement() {
-		t.Log("Element:", ele.Key, ele.Value)
-	}
-	t.Log("Success")
-}
-
-type TestMap struct {
-	gomap       map[interface{}]interface{}
-	cmap        ConcurrencyMap
-	gomapnolock map[interface{}]interface{}
-	sync.RWMutex
-}
-
-func NewTestMap() *TestMap {
-	return &TestMap{gomap: make(map[interface{}]interface{}), cmap: NewConcurrencyMap(), gomapnolock: make(map[interface{}]interface{}, 1024*1024)}
-}
-func (m *TestMap) GomapNolockGetSet(key interface{}, value interface{}) bool {
-
-	m.gomap[key] = value
-
-	runtime.Gosched()
-
-	newvalue := m.gomap[key]
-
-	return newvalue == value
-
-}
-func (m *TestMap) GomapGetSet(key interface{}, value interface{}) bool {
-	m.Lock()
-	m.gomap[key] = value
-	m.Unlock()
-	runtime.Gosched()
-	m.RLock()
-	newvalue := m.gomap[key]
-	m.RUnlock()
-	return newvalue == value
-
-}
-func (m *TestMap) ConcurrencymapGetSet(key interface{}, value interface{}) bool {
-	err := m.cmap.Set(key, value)
+	cmap.Set("Foo", "bar")
+	cmap.Set("Foo1", "bar1")
+	cmap.Set("Foo2", "bar2")
+	cmap.SetIfAbsent("Foo", "bar2")
+	foo, err := cmap.Get("Foo")
 	if err != nil {
-		return false
-	}
-	newvalue, _ := m.cmap.Get(key)
-	return newvalue == value
-}
-
-func TestConcurrencyMap(t *testing.T) {
-	cmap := NewConcurrencyMap()
-	err := cmap.Set("foo", "bar")
-	if err != nil {
-		t.Error("Set error:", err)
+		t.Error(err)
 		return
 	}
-	val, err := cmap.Get("foo")
-	if err != nil {
-		t.Error("Get error:", err)
-		return
+	t.Log("Foo:", foo)
+	t.Log("Keys:", cmap.Keys())
+	t.Log("Values:", cmap.Values())
+	t.Log("Map:", cmap.ToMap(), ",Len:", cmap.Len())
+	foo2, err := cmap.Remove("Foo2")
+	t.Log("Remove value:", foo2)
+	t.Log("Map:", cmap.ToMap(), ",Len:", cmap.Len())
+}
+
+func TestCMapElements(t *testing.T) {
+	cmap := NewConcurrencyMap()
+	for i := 0; i < 10; i++ {
+		err := cmap.Set(fmt.Sprintf("Foo_%d", i), i)
+		if err != nil {
+			t.Error(err)
+			return
+		}
 	}
-	t.Log("Foo value:", val)
-	t.Log("Map value:", cmap.ToMap(), ",Map len:", cmap.Len())
-}
-
-func BenchmarkNolockGoMap(b *testing.B) {
-	b.StopTimer()
-	testmap := NewTestMap()
-	b.StartTimer()
-	var i int64 = 0
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			n := atomic.AddInt64(&i, 1)
-			var key = fmt.Sprintf("foo_%d", n)
-			result := testmap.GomapGetSet(key, n)
-			if !result {
-				b.Error("执行错误错误结果")
-			}
-		}
-	})
-}
-
-func BenchmarkGoMap(b *testing.B) {
-	b.StopTimer()
-	testmap := NewTestMap()
-	b.StartTimer()
-	var i int64 = 0
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			n := atomic.AddInt64(&i, 1)
-			var key = fmt.Sprintf("foo_%d", n)
-			result := testmap.GomapGetSet(key, n)
-			if !result {
-				b.Error("执行错误错误结果")
-			}
-		}
-	})
-}
-
-func BenchmarkConcurrencyMap(b *testing.B) {
-	b.StopTimer()
-	testmap := NewTestMap()
-	b.StartTimer()
-	var i int64 = 0
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			n := atomic.AddInt64(&i, 1)
-			var key = fmt.Sprintf("foo_%d", n)
-			result := testmap.ConcurrencymapGetSet(key, n)
-			if !result {
-				b.Error("执行错误错误结果")
-			}
-		}
-	})
-
+	for element := range cmap.Elements() {
+		t.Log("Key:", element.Key, ",Value:", element.Value)
+	}
 }
